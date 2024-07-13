da4b56e865839aaeba7645b3179eb47728fbe2d4
rimap <fairy3@gmail.com>
2024-07-13 15:45:23 +0300

    Fix scripts

--- vars/greet.groovy
+++ vars/greet.groovy
@@ -1,8 +1,3 @@
 def call() {
-    def user = currentBuild.rawBuild.getCause(hudson.model.Cause$UserIdCause)?.userId
-    if (user) {
-        echo "Hello, ${user}!"
-    } else {
-        echo "Hello, World!"
-    }
-}    
+    echo "Hello, World!"
+}
