492ce74bf45d053e4d89c774ff2180e0ca5b688b
rimap <fairy3@gmail.com>
2024-07-13 15:49:56 +0300

    Fix scripts

--- vars/nexusLogin.groovy
+++ vars/nexusLogin.groovy
@@ -1,5 +1,5 @@
 def call(String credentialsId, String nexusProtocol, String nexusUrl, String nexusRepo) {
-   withCredentials([usernamePassword(credentialsId: credentialId, usernameVariable: 'NEXUS_USERNAME', passwordVariable: 'NEXUS_PASSWORD')]) {
+   withCredentials([usernamePassword(credentialsId: credentialsId, usernameVariable: 'NEXUS_USERNAME', passwordVariable: 'NEXUS_PASSWORD')]) {
         echo "Logging in to Nexus as ${NEXUS_USERNAME}"
         sh "docker login -u ${NEXUS_USERNAME} -p ${NEXUS_PASSWORD} ${nexusProtocol}://${nexusUrl}/repository/${nexusRepo}"
    }
