cc6df79b8f2da5c43d296b6d8be37990d08d0d68
rimap <fairy3@gmail.com>
2024-07-13 18:26:58 +0300

    Fix script

--- src/org/UserUtils.groovy
+++ src/org/UserUtils.groovy
@@ -2,7 +2,7 @@ package org
 
 class UserUtils {
     static String getBuildUserName(script) {
-       def userName = script.env.BUILD_USER_ID
+       def userName = script.env.BUILD_USER
        return userName ?: 'unknown user'
     }
 }
