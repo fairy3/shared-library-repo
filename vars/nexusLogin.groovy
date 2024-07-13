60d2cf84ab3dbdcb59996bc5d8ef822d631ee20c
rimap <fairy3@gmail.com>
2024-07-13 15:36:51 +0300

    Fix scripts

--- vars/nexusLogin.groovy
+++ vars/nexusLogin.groovy
@@ -4,3 +4,4 @@ def call(String credentialsId, String nexusProtocol, String nexusUrl, String nex
         sh "docker login -u ${NEXUS_USERNAME} -p ${NEXUS_PASSWORD} ${nexusProtocol}://${nexusUrl}/repository/${nexusRepo}"
    }
 }
+
