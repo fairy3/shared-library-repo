f05a16decaab426760e5766c5e6ae4ae2e488ca8
rimap <fairy3@gmail.com>
2024-07-13 16:57:36 +0300

    Fix scripts

--- src/org/UserUtils.groovy
+++ src/org/UserUtils.groovy
@@ -1,11 +1,10 @@
 package org
 
-import hudson.model.User
-
 class UserUtils {
     static String getBuildUserName(script) {
         static String getBuildUserName(script) {
-        def userName = script.env.BUILD_USER_ID
-        return userName ?: 'unknown user'
+            def userName = script.env.BUILD_USER_ID
+            return userName ?: 'unknown user'
+        }
     }
 }
